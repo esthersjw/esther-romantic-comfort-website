6e03d96f3d4bca9923cdbbccba6859dc5442cf5e
Andrew Woan <andrewwoan@gmail.com>
2026-05-21 13:44:06 -0400

    heyo

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ infoBtn.addEventListener("click", () => {
     "Information & Credits",
     `This is a prototype comfort web experience for couples therapy. Digital mental health interventions often have high attrition rates (i.e., people stop using digital solutions quickly over time). This website is an experiment to personalize these solutions in more creative/playful ways to help keep retention up to the point where the website or the solutions in the website are no longer needed. Of course, depending on the day, you might swing more avoidant or anxious and it doesn't necessarily mean you have an insecure attachment style; so don't take everything in this website as "binary" it's more of just general patterns.
 <br><br>
-Click around to explore!! For a full set of credits, see the <a href="https://github.com/andrewwoan/john-and-patricias-romantic-comfort-website" target="_blank" rel="noopener">GitHub Repository</a> and a <a href="https://www.youtube.com/watch?v=w2MnkhTGJQA" target="_blank" rel="noopener">YouTube tutorial</a> on how to create a comfort website.`,
+Click around to explore!! For a full set of credits, see the <a href="https://github.com/andrewwoan/john-and-patricias-romantic-comfort-website" target="_blank" rel="noopener">GitHub Repository</a> and a <a href="https://www.youtube.com/watch?v=w2MnkhTGJQA" target="_blank" rel="noopener">YouTube tutorial</a> on how to create a comfort website with little technical knowledge required.`,
   );
 });
 
